679723272b76ab291bfda14fbe385b741804b450
mainak90 <mainak90@gmail.com>
2021-02-12 23:31:40 +0100

    Return a full backend line

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -32,46 +31,23 @@ usage call:  ./haproxy-slot-calculator <servicename>
 
 	env_var := os.Getenv("CONSUL_TEMPLATE_OPTS")
 	consul_host := strings.Split(env_var, "=")[1]
-	url := fmt.Sprintf("http://%s/v1/catalog/service/%s", consul_host, os.Args[1])
+	clustername := os.Getenv("ECS_CLUSTER")
 
-	spaceClient := http.Client{
-		Timeout: time.Second * 5, // 5 secs
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	var defaultResponse string = fmt.Sprintf("server-template %s 5 _%s._tcp.service.consul resolvers consul check inter 5s", os.Args[1], os.Args[1])
+	serviceResponse, err := request(consul_host, os.Args[1])
 	if err != nil {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("10"))
+		fmt.Fprintln(os.Stdout, fmt.Sprintf(defaultResponse))
 		fmt.Fprintln(os.Stderr, fmt.Sprintf("%s", err))
 		os.Exit(1)
 	}
-	res, err := spaceClient.Do(req)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("10"))
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("%s", err))
-		os.Exit(1)
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("10"))
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("%s", err))
-		os.Exit(1)
-	}
-	var lAnything []map[string]interface{}
-	err = json.Unmarshal(body, &lAnything)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("10"))
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("%s", err))
-		os.Exit(1)
-	}
-	finalVal := PersistedCounter(os.Args[1], len(lAnything))
-	iszero := IsZeroOfUnderlyingType(finalVal)
-	if (iszero) {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("10"))
-		os.Exit(0)
-	} else {
-		fmt.Fprintln(os.Stdout, fmt.Sprintf("%s", finalVal))
+	if !isRenderable(serviceResponse, clustername) {
+		fmt.Fprintln(os.Stdout, fmt.Sprintf(""))
 		os.Exit(0)
 	}
+
+	finalVal := PersistedCounter(os.Args[1], len(serviceResponse))
+	fmt.Fprintln(os.Stdout, fmt.Sprintf(makeResponse(os.Args[1], finalVal)))
+	os.Exit(0)
 }
 
 func PersistedCounter(servicename string, len int) string {
@@ -84,15 +60,15 @@ func PersistedCounter(servicename string, len int) string {
 		os.Create(servicesPath)
 	}
 	var strlen string
-	if len < 10 {
-		strlen = strconv.Itoa(10)
+	if len < 5 {
+		strlen = strconv.Itoa(5)
 	} else {
 		strlen = strconv.Itoa(20)
 	}
 	jsonFile, err := os.Open(servicesPath)
 	// if we os.Open returns an error then handle it
 	if err != nil {
-		return "10"
+		return "5"
 	}
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
@@ -104,11 +80,11 @@ func PersistedCounter(servicename string, len int) string {
 			result[servicename] = strlen
 			dataBytes, err := json.Marshal(result)
 			if err != nil {
-				return "10"
+				return "5"
 			}
 			err = ioutil.WriteFile(servicesPath, dataBytes, 0777)
 			if err != nil {
-				return "10"
+				return "5"
 			}
 			return strlen
 		}
@@ -116,19 +92,51 @@ func PersistedCounter(servicename string, len int) string {
 	result[servicename] = strlen
 	dataBytes, err := json.Marshal(result)
 	if err != nil {
-		return "10"
+		return "5"
 	}
 	err = ioutil.WriteFile(servicesPath, dataBytes, 0777)
 	if err != nil {
-		return "10"
+		return "5"
 	}
 	return strlen
 }
 
-func IsZeroOfUnderlyingType(x interface{}) bool {
-	if reflect.DeepEqual("0", x) {
-		return true
-	} else {
-		return false
+func request(host string, service string) ([]map[string]interface{}, error) {
+	url := fmt.Sprintf("http://%s/v1/catalog/service/%s", host, service)
+	spaceClient := http.Client{
+		Timeout: time.Second * 5, // 5 secs
 	}
+	// Just for the sake of returning an empty datatype
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := spaceClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var lAnything []map[string]interface{}
+	err = json.Unmarshal(body, &lAnything)
+	return lAnything, nil
+}
+
+func isRenderable(servicedef []map[string]interface{}, clustername string) bool {
+	tagmap := servicedef[0]["ServiceTags"]
+	tagstring := fmt.Sprintf("%v", tagmap)
+	tagtrim := strings.Trim(strings.Trim(tagstring, "["), "]")
+	tagslice := strings.Split(tagtrim, " ")
+	for _, v := range tagslice {
+		if v == clustername {
+			return true
+		}
+	}
+	return false
+}
+
+func makeResponse(servicename string, slot string) string {
+	return fmt.Sprintf("server-template %s %s _%s._tcp.service.consul resolvers consul check inter 5s", servicename, slot, servicename)
 }
